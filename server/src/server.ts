7048af116a0c65b2dd85a3c7e5d1034b0ccff07b
Ricardo Pellicioli <ricardo@bettha.com>
2023-05-16 12:28:02 -0300

    prisma start and get users

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.listen({
-  port: 3001
-}).then(() => {
-  console.log('👌 HTTP server running on http://localhost:3001');
+app.get('/users', async () => {
+  const users = prisma.user.findMany();
+
+  return users;
 });
+
+app
+  .listen({
+    port: 3001
+  })
+  .then(() => {
+    console.log('👌 HTTP server running on http://localhost:3001');
+  });
