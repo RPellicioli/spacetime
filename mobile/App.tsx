697010150173a514e37ffb0cc80f86ec1dcf0c5c
Ricardo Pellicioli <ricardo@bettha.com>
2023-05-18 15:40:32 -0300

    home-page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,67 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { styled } from "nativewind";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
+import blurBg from "./src/assets/images/blur.png";
+import Stripes from "./src/assets/svg/stripes.svg";
+import NLWLogo from "./src/assets/svg/logo.svg";
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={blurBg}
+      className="y-10 relative flex-1 items-center bg-gray-900 px-8"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="font-title font-2xl text-center leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+
+          <Text className="font-body text-center text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="roudend-full bg-green-500 px-4 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="font-body text-center text-sm leading-relaxed text-gray-200">
+        Feito por Ricardo Pellicioli
+      </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
