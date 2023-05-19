cd6c3648cb9e997b8009407266d3426402d9aa71
Ricardo Pellicioli <ricardo@bettha.com>
2023-05-19 13:41:14 -0300

    jwt and auth routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,23 @@
+import 'dotenv/config';
+
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
 import { userRoutes } from './routes/users';
 import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 const app = fastify();
 
 app.register(cors, {
   origin: true
 });
-app.register(userRoutes, memoriesRoutes);
+app.register(jwt, {
+  secret: 'spacetime'
+});
+app.register(userRoutes);
+app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app
   .listen({
