3660d0dc361baf3d7ece9cfa6f4a4d9c07ba9e3a
Ricardo Pellicioli <ricardo@bettha.com>
2023-05-20 19:28:34 -0300

    create middleware routes and logout

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -4,11 +4,12 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get('code');
+  const redirectTo = request.cookies.get('redirectTo')?.value;
 
   const response = await api.post('/register', { code });
   const { token } = response.data;
 
-  const redirectURL = new URL('/', request.url);
+  const redirectURL = redirectTo ?? new URL('/', request.url);
   const expiresInSeconds = 60 * 60 * 24 * 30;
 
   return NextResponse.redirect(redirectURL, {
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import { ReactNode } from 'react';
 import './globals.css';
+import { cookies } from 'next/headers';
+import { Copyright } from '@/components/Copyright';
+import { Hero } from '@/components/Hero';
+import { SignIn } from '@/components/SignIn';
+import { Profile } from '@/components/Profile';
+
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree
 } from 'next/font/google';
-
+import { EmptyMemory } from '@/components/EmptyMemory';
 const robotoFont = Roboto({ subsets: ['latin'], variable: '--font-sans' });
 const altFont = BaiJamjuree({
   subsets: ['latin'],
@@ -19,12 +25,31 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token');
+
   return (
     <html lang="en">
       <body
         className={`${robotoFont.variable} ${altFont.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          <div className="relative flex flex-1 flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/svg/stars.svg)] bg-cover px-28 py-16">
+            <div
+              className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full
+         bg-purple-700 opacity-50 blur-full"
+            ></div>
+
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col bg-[url(../assets/svg/stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
