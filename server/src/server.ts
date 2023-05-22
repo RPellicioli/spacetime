a90e6b9a800edd94636d24ce5a642c60142ea667
Ricardo Pellicioli <ricardo@bettha.com>
2023-05-21 21:28:55 -0300

    API upload file

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,16 +3,24 @@ import 'dotenv/config';
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import jwt from '@fastify/jwt';
+import multipart from '@fastify/multipart';
+import { fastifyStatic } from '@fastify/static';
+import { resolve } from 'node:path';
 import { userRoutes } from './routes/users';
 import { memoriesRoutes } from './routes/memories';
 import { authRoutes } from './routes/auth';
+import { uploadRoutes } from './routes/upload';
 
 const app = fastify();
 
+app.register(multipart);
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+});
 app.register(cors, {
   origin: true
 });
-
 app.register(jwt, {
   secret: 'spacetime'
 });
@@ -20,10 +28,12 @@ app.register(jwt, {
 app.register(authRoutes);
 app.register(userRoutes);
 app.register(memoriesRoutes);
+app.register(uploadRoutes);
 
 app
   .listen({
-    port: 3001
+    port: 3001,
+    host: '0.0.0.0'
   })
   .then(() => {
     console.log('👌 HTTP server running on http://localhost:3001');
